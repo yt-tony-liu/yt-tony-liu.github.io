3d42cf0b3ca13d88900bfd46d60f98822c7ae424
yt-tony-liu <71209514+yt-tony-liu@users.noreply.github.com>
2026-03-14 17:14:14 +0000

    Deploying to gh-pages from @ yt-tony-liu/yt-tony-liu.github.io@05565b43659e6978684287a011dffcc5995f10fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A growing collection of some cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
